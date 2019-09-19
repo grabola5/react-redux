30e4aae8f9c83c0c98f012cd77e0184eb403ad69
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-19 22:02:37 +0200

    Init project

---
+++ 19.7-react_redux/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Tu będzie lista komentarzy!
+    </div>
+  );
+}
+
+export default App;
