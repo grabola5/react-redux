9e1b104770b98782cbf7f582f1cceb010dab1504
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-20 21:32:30 +0200

    Add component CommentsListContainer

---
+++ 19.7-react_redux/src/CommentsListContainer.js
@@ -0,0 +1,10 @@
+//funkcja connect służy do łączenia komponentów do store
+import {connect} from 'react-redux';
+import CommentsList from '/CommentsList';
+
+//obiekt, który zwraca mapStateToProps, podpina konkretne wartości props do CommentsList
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+export default connect(mapStateToProps)(CommentsList);
