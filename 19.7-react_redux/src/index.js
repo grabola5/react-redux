003bc55811ee8bbe81c76cb694812d602010f73f
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-20 21:53:15 +0200

    Enclose reducer

--- 19.7-react_redux/src/index.js
+++ 19.7-react_redux/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducer'
 
 const store = createStore(reducer);
 
